27fc327436939f22ffbb8b28d9987d7eee005829
chougulesurajb <chougulesurajb@gmail.com>
2020-11-11 12:06:10 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,23 +1,53 @@
-var express = require('express');
-var app = express();
+console.log("starting app");
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+const fs = require("fs");
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+var dir = path.join(__dirname, "build");
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+var mime = {
+  html: "text/html",
+  txt: "text/plain",
+  css: "text/css",
+  gif: "image/gif",
+  jpg: "image/jpeg",
+  png: "image/png",
+  svg: "image/svg+xml",
+  js: "application/javascript",
+};
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // to enable calls from every domain
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  ); // allowed actiosn
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-// set the home page route
-app.get('/', function(req, res) {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // to deal with chrome sending an extra options request
+  }
 
-    // ejs render automatically looks in the views folder
-    res.render('index');
+  next(); // call next middlewer in line
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.get("*", function (req, res) {
+  var file = path.join(dir, req.path.replace(/\/$/, "/index.html"));
+  if (file.indexOf(dir + path.sep) !== 0) {
+    return res.status(403).end("Forbidden");
+  }
+  var type = mime[path.extname(file).slice(1)] || "text/plain";
+  var s = fs.createReadStream(file);
+  s.on("open", function () {
+    res.set("Content-Type", type);
+    s.pipe(res);
+  });
+  s.on("error", function () {
+    res.set("Content-Type", "text/plain");
+    res.status(404).end("Not found");
+  });
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
